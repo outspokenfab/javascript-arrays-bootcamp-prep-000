88bb8ddce8c3e744956dbfdc4d42059727c024c8
curious-metric-5246 <richardjharvey1@gmail.com>
2020-03-06 18:49:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -50,8 +50,8 @@ function destructivelyRemoveElementFromBeginningOfArray(sampleArray, element){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
+  array2 = array.shift();
+  return array2;
 }
 
 
