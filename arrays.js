98b5f27a7ac052b8a0ec26a079bb8ecc443c55e5
curious-metric-5246 <richardjharvey1@gmail.com>
2020-03-06 18:42:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,7 @@ function accessElementInArray(sampleArray, index){
 
 
 function addElementToEndOfArray(addArray, element){
+  var array = addArray;
   var array2 = [...array, element];
   return array2;
 }
